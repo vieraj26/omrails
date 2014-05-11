e76c855b009b7c9feca459b03f07231cf5eb61e9
joed viera <vieraj26@gmail.com>
2014-05-11 00:37:18 -0400

    trial part43

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root_path -> '/'
 	root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
